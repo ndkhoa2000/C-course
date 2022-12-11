9acda2082bf0b6cccabb575ba8e79d0a908ff414
ndkhoa2000 <gregnguyen610@gmail.com>
2022-12-11 22:24:32 +0700

    add

---
+++ LAB2/sign_determination/sign.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a,b;
+    cin >> a >> b ;
+    if (a*b>0) { 
+        cout << "YES"<< endl;
+    }
+    else { 
+        cout << "NO" << endl;
+    }
+    return 0;
+}
